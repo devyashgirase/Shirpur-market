8024f8fa7349397f761d8796711e3ad6ca3dfc49
Yash <yashpal.com>
2025-10-30 12:06:30 +0530

    dad

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,11 @@
+// Fix for production headers error
+if (typeof globalThis !== 'undefined') {
+  globalThis.global = globalThis.global || {};
+  if (!globalThis.global.headers) {
+    globalThis.global.headers = {};
+  }
+}
+
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
